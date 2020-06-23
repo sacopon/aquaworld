eab5532284a6f61297528500b4a36a884ea67d84
Takeshi Sakurai <sacopon.dev@gmail.com>
2020-06-24 00:07:49 +0900

    :sparkles: install and setting pixi.js

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+import * as PIXI from "pixi.js";
+import {
+  disableTouchEvent,
+  disableOuterCanvasTouchEvent,
+} from "application/utils/disable_touch_event";
+import { getWindowSizeAsync } from "application/utils/get_window_size_async";
+
+async function mainProgram() {
+  debugger;
+  disableOuterCanvasTouchEvent();
+  const size = await getWindowSizeAsync();
+  console.info(size);
+  const app = new PIXI.Application({
+    width: size.width,
+    height: size.height,
+    transparent: false,
+  });
+  disableTouchEvent(app.view);
+}
+
+window.addEventListener("DOMContentLoaded", mainProgram);
